a31ad2207f87cc279ddc8cc48e1790e9316abba6
Yousef <yhadhr@gmail.com>
2017-11-29 07:45:09 +0300

    [Code] added cache forecast data code

--- scripts/app.js
+++ scripts/app.js
@@ -169,6 +169,24 @@ document.getElementById('butAddCity').addEventListener('click', function() {
     var url = 'https://query.yahooapis.com/v1/public/yql?format=json&q=' +
         statement + ' and u =\'c\'';
     // TODO add cache logic here
+	    if ('caches' in window) {
+      /*
+       * Check if the service worker has already cached this city's weather
+       * data. If the service worker has the data, then display the cached
+       * data while the app fetches the latest data.
+       */
+      caches.match(url).then(function(response) {
+        if (response) {
+          response.json().then(function updateFromCache(json) {
+            var results = json.query.results;
+            results.key = key;
+            results.label = label;
+            results.created = json.query.created;
+            app.updateForecastCard(results);
+          });
+        }
+      });
+    }
 
     // Fetch the latest data.
     var request = new XMLHttpRequest();
